83df8f06ebcbdb37954f9079514cd410f871fac2
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2022-12-31 15:31:33 +0700

    tambahan 5 hari cuaca

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
             +"&units=metric&appid=7f27214484dc61d89cc71da5d61378ec"
+            
             )
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
@@ -15,7 +16,7 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         document.querySelector(".city").innerText = "Cuaca di " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
         document.querySelector(".desc").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humid").innerText = "Kelembapan: " + humidity + "%";
