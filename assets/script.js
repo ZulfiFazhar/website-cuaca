b3044699aae9f0dadf6194d4be2ecb2f958f3674
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2023-01-03 11:21:59 +0700

    penyesuaian fitur

--- assets/script.js
+++ assets/script.js
@@ -33,11 +33,11 @@ document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
 });
 
-document.querySelector(".search-bar").addEventListener("keyup", function(event){
-    if (event.key == "Enter") {
-        weather.search();
-    }
-});
+// document.querySelector(".search-bar").addEventListener("keyup", function(event){
+//     if (event.key == "Enter") {
+//         weather.search();
+//     }
+// });
 
 weather.fetchWeather("Bandung");
 
